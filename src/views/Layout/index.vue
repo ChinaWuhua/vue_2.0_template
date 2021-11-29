0808d02305c275e1ad6dbf24e8e6a1fb144160bf
吴华 <wuhua@sinobest.cn>
2021-11-29 16:17:26 +0800

    完善代码

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    index
-    <router-view/>
+  <div class="v-body">
+    <div class="v-sider">
+      <Sider></Sider>
+    </div>
+    <div class="v-container">
+      <div class="v-top">
+        <Top></Top>
+      </div>
+      <div class="v-wrap">
+        <router-view/>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import Sider from './sider.vue'
+import Top from './top.vue'
+export default {
+  components: {
+    Sider, Top
+  }
+}
+</script>
+
+<style lang="less" src="./index.less" scoped></style>
